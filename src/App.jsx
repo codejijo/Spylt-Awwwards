71265354bd90d5f0b6a58d768d36a78e5fcc044f
Codejijo <jijojohnlikeu@gmail.com>
2025-08-04 01:19:32 +0530

    feat : added static hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import HeroSection from './sections/HeroSection'
 
 const App = () => {
   return (
-    <div className='border-red-500 text-5xl'>App</div>
+    <main>
+      <NavBar />
+      <HeroSection />
+    </main>
   )
 }
 
